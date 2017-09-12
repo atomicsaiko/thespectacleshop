ed8cd59e3f82b65e84165b9dc136598b9b243688
Jan-Sijmen <js.zwarts@gmail.com>
2017-09-12 13:45:25 +0200

    Adds profile model with reference to user

--- db/migrate/20170912114411_create_profiles.rb
+++ db/migrate/20170912114411_create_profiles.rb
@@ -6,6 +6,7 @@ class CreateProfiles < ActiveRecord::Migration[5.1]
       t.string :postcode
       t.string :city
       t.string :country
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912101509) do
+ActiveRecord::Schema.define(version: 20170912114411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,8 +21,10 @@ ActiveRecord::Schema.define(version: 20170912101509) do
     t.string "postcode"
     t.string "city"
     t.string "country"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +44,5 @@ ActiveRecord::Schema.define(version: 20170912101509) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
